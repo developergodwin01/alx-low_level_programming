3fe23efd8b929c6b6cc5d37bd39732c1389fdc61
deverlopergodwin <godwinovat.go@gmail.com>
2022-10-16 15:41:56 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <unistd.h>
+
+/**
+ * main - print exactly and the piece of art is useful
+ *
+ * Return: Always 1.
+ */
+
+int main(void)
+
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}	
