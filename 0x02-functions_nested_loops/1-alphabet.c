950c5f6f2d15b43aa3626dbf977d3b87c0025682
deverlopergodwin <godwinovat.go@gmail.com>
2022-10-19 04:02:47 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet - print the alpahabet in lowercase
+ * print_alphabet - print the alpahabet in lowercase,followed by a new line
  */
 void print_alphabet(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 			_putchar('\n');
